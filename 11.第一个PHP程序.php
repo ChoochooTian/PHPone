a3856c88d45c72bc6ad8910b3c9fe783aeebb76d
田松岩 <3366361099@qq.com>
2017-05-26 22:23:34 +0800

    Create 11.第一个PHP程序.php

---
+++ 11.第一个PHP程序.php
@@ -0,0 +1,13 @@
+	<?php
+    //第一个php程序,注释
+    /*输出hello php！*/  
+    echo "hello world!";  
+    
+//引用、起别名
+// $userName="zzm";
+// $userNameR=&$userName;
+// $userName='zhangzhimin';
+
+// echo $userNameR;//zhangzhimin
+// var_dump($userName);//string(11) "zhangzhimin"
+	?>
